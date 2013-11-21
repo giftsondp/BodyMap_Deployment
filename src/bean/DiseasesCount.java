75ec641be02f953d4e4da5a280f54c2d92aa57d2
Giftson David Paul Jebasingh <giftsonp@gmail.com>
2013-11-21 01:55:53 -0800

    Latest added

---
+++ src/bean/DiseasesCount.java
@@ -0,0 +1,19 @@
+package bean;
+
+public class DiseasesCount {
+	private int count;
+	private String diseaseName;
+	
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getDiseaseName() {
+		return diseaseName;
+	}
+	public void setDiseaseName(String diseaseName) {
+		this.diseaseName = diseaseName;
+	}
+}
